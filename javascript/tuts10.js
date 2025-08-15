589ba15c154af045c0a35ee0107b9b88a0777266
s4vvvvv <sbrlzxc@gmail.com>
2025-08-15 20:35:16 +0800

    added tuts10

---
+++ javascript/tuts10.js
@@ -0,0 +1,27 @@
+let human = [
+  {
+    firstName: "Sabriel",
+    lastName: "San Agustin",
+    age: "19",
+  },
+  {
+    firstName: "Aeron",
+    lastName: "Martinez",
+    age: "19",
+  },
+  {
+    firstName: "Khing",
+    lastName: "Ulpato",
+    age: "18",
+  }
+];
+
+
+for(let i = 0;i < human.length;i++) {
+console.log(`First Name : ${human[i].firstName}`);
+console.log(`Last Name  : ${human[i].lastName}`);
+console.log(`Age        : ${human[i].age}`);
+console.log(" ");
+}
+
+//JSON | Parsing | Stringify

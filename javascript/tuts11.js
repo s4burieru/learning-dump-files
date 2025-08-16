43ec054626fb0b814e7a52babe48f03c1a6c658c
s4vvvvv <sbrlzxc@gmail.com>
2025-08-16 11:32:32 +0800

    format fix

--- javascript/tuts11.js
+++ javascript/tuts11.js
@@ -1,19 +1,19 @@
 let grado = {
-    english: 95,
-    math: 80,
-    science: 89,
-    filipino: 92,
-    pe: 95,
+  english: 95,
+  math: 80,
+  science: 89,
+  filipino: 92,
+  pe: 95,
+};
+
+let ave = 0;
+
+for (let i in grado) {
+  console.log(`${i}: ${grado[i]}`);
+  ave += grado[i];
 }
 
-let ave = 0
-
-for(let i in grado) {
-console.log(`${i}: ${grado[i]}`);
-ave += grado[i];
-}
-
-ave /= Object.keys(grado).length
+ave /= Object.keys(grado).length;
 console.log(`Averege: ${ave}`);
 
 // JSON & FOR-IN LOOP

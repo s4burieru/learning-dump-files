d8fd78c41cfe87cc7640e7ba939f2ccbd53de71c
s4vvvvv <sbrlzxc@gmail.com>
2025-08-17 10:23:25 +0800

    added tuts12

---
+++ javascript/tuts12.js
@@ -0,0 +1,11 @@
+function addition(arrNum) {
+  let sum = 0;
+  for (let i = 0; i < arrNum.length; i++) {
+    sum += arrNum[i];
+  }
+  return sum;
+}
+let total = addition([5,5,5,5]);
+console.log(total);
+
+// Functions

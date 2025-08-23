0b271b50f5889ad64b2f64b28482d1929c96893d
s4vvvvv <sbrlzxc@gmail.com>
2025-08-23 11:29:42 +0800

    added tuts13

---
+++ javascript/tuts13.js
@@ -0,0 +1,25 @@
+let body = document.querySelector("body");
+body.style.backgroundColor = "Gray";
+
+let div = document.querySelector("div");
+div.style.height = "300px";
+div.style.backgroundColor = "Black";
+div.style.textAlign = "center";
+div.style.paddingTop = "60px";
+
+let img = document.querySelector("img");
+img.src = "20250810_154307.jpg";
+img.style.width = "150px";
+
+let h1 = document.querySelector("h1");
+h1.innerHTML = "Sbriel Adriel";
+h1.style.color = "White";
+h1.style.fontFamily = "Arial";
+
+let p = document.querySelector("p");
+p.innerHTML =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quis, quasi ducimus magnam asperiores ab suscipit rerum sint illo, et rem architecto fugiat! Magnam, aliquam. Libero obcaecati ex facilis distinctio";
+p.style.color = "White";
+p.style.fontFamily = "Arial";
+p.style.fontSize = "19px";
+p.style.textAlign = "Center";

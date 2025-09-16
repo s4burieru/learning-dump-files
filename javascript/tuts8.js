296ba86ebb7674b692c0bff0aae83bed1301efa1
s4vvvvv <sbrlzxc@gmail.com>
2025-09-16 09:40:38 +0800

    format docs

--- javascript/tuts8.js
+++ javascript/tuts8.js
@@ -1,12 +1,11 @@
-let arr = [5,5,5,5,5];
+let arr = [5, 5, 5, 5, 5];
 let add = 0;
 let i = 0;
 
-while(arr[i]){
-   add += arr[i];
-   i++
-  
+while (arr[i]) {
+  add += arr[i];
+  i++;
 }
- console.log(add);
+console.log(add);
 
- // WHILE | DO-WHILE LOOP | BREAK
+// WHILE | DO-WHILE LOOP | BREAK

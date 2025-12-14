ae36e10a033cf15a552cc28f32a75e65a1fffd7d
s4vvvvv <sbrlzxc@gmail.com>
2025-12-14 20:05:00 +0800

    Refactor keydown event listener for improved readability

--- javascript/SuperSimpleDev/scripts/rock-paper-scissors-with-style.js
+++ javascript/SuperSimpleDev/scripts/rock-paper-scissors-with-style.js
@@ -24,14 +24,14 @@ document.querySelector(".js-scissors-button").addEventListener("click", () => {
   playGame("Scissors");
 });
 
-document.body.addEventListener('keydown', (event) => {
-if (event.key === 'r') {
-  playGame('Rock');
-} else if (event.key === 'p') {
-  playGame('Paper');
-} else if (event.key === 's') {
-  playGame('Scissors');
-}
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "r") {
+    playGame("Rock");
+  } else if (event.key === "p") {
+    playGame("Paper");
+  } else if (event.key === "s") {
+    playGame("Scissors");
+  }
 });
 
 function playGame(playerMove) {
